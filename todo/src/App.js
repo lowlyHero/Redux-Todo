cac3e477ba49c02235ce986ae0ce824747ae5fa9
lowlyHero <e.nunn1009@gmail.com>
2019-06-06 17:53:18 -0400

    Got todos and todoList fully functional with form to add and delete todos

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from './components/Todos';
+import Addtodo from './components/Addtodo';
 
 class App extends Component {
     state = {
@@ -17,12 +18,19 @@ class App extends Component {
             todos
         })
     }
-
+    addTodo = (todo) => {
+        todo.id = Date.now();
+        let todos = [...this.state.todos, todo];
+        this.setState({
+            todos
+        })
+    }
     render() {
     return (
       <div className='App container'>
           <h1>Todo's</h1>
           <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+          <Addtodo addTodo={this.addTodo} />
       </div>
     );
   }
